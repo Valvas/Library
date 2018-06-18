5360f0c7813ac75ffd58afa93cf3d0e43d2acef9
Valvas <valvas.vincent@outlook.fr>
2018-06-18 09:19:06 +0200

    News added

--- public/js/navigation/account.js
+++ public/js/navigation/account.js
@@ -1,22 +1,40 @@
 /****************************************************************************************************/
 
-document.getElementById('navigation-account-icon').addEventListener('click', openOrCloseAccountList);
+$(window).click(() => { closeAccountMenu(); });
+  
+$('#navigation-account-icon').click((event) => { event.stopPropagation(); });
+$('#navigation-account-list').click((event) => { event.stopPropagation(); });
+
+document.getElementById('navigation-account-icon').addEventListener('click', openAccountMenu);
 
 /****************************************************************************************************/
 
-function openOrCloseAccountList(event)
+function openAccountMenu(event)
 {
-  if(document.getElementById('navigation-account-list').hasAttribute('style'))
-  {
-    document.getElementById('navigation-account-list').removeAttribute('style');
-    document.getElementById('navigation-account-icon').removeAttribute('style');
-  }
+  closeMenu();
+  
+  document.getElementById('navigation-account-icon').removeEventListener('click', openAccountMenu);
 
-  else
+  $(document.getElementById('navigation-account-list')).slideDown(250, () =>
   {
-    document.getElementById('navigation-account-list').style.display = 'block';
     document.getElementById('navigation-account-icon').style.color = '#7EDA7E';
-  }
+
+    document.getElementById('navigation-account-icon').addEventListener('click', closeAccountMenu);
+  });
+}
+
+/****************************************************************************************************/
+
+function closeAccountMenu(event)
+{
+  document.getElementById('navigation-account-icon').removeEventListener('click', closeAccountMenu);
+
+  $(document.getElementById('navigation-account-list')).slideUp(250, () =>
+  {
+    document.getElementById('navigation-account-icon').removeAttribute('style');
+
+    document.getElementById('navigation-account-icon').addEventListener('click', openAccountMenu);
+  });
 }
 
 /****************************************************************************************************/
--- public/js/navigation/menu.js
+++ public/js/navigation/menu.js
@@ -1,56 +1,83 @@
 /****************************************************************************************************/
 
+$(window).click(() => { closeMenu(); });
+  
+$('#navigation-menu-list').click((event) => { event.stopPropagation(); });
+$('#navigation-menu-icon').click((event) => { event.stopPropagation(); });
+
 if(document.getElementById('navigation-apps-icon'))
 {
-  document.getElementById('navigation-apps-icon').addEventListener('click', openOrCloseApps);
+  document.getElementById('navigation-apps-icon').addEventListener('click', openMenuAppsList);
 }
 
 if(document.getElementById('navigation-menu-icon'))
 {
-  document.getElementById('navigation-menu-icon').addEventListener('click', openOrCloseMenuList);
+  document.getElementById('navigation-menu-icon').addEventListener('click', openMenu);
 }
 
 /****************************************************************************************************/
 
-function openOrCloseMenuList(event)
+function openMenu(event)
 {
-  if(document.getElementById('navigation-menu-list').hasAttribute('style'))
-  {
-    if(document.getElementById('navigation-apps-icon'))
-    {
-      document.getElementById('navigation-apps-icon').removeAttribute('style');
-      document.getElementById('navigation-apps-icon').children[1].innerHTML = `<i class='fas fa-angle-down'></i>`;
-      document.getElementById('navigation-apps-list').removeAttribute('style');
-    }
-    
-    document.getElementById('navigation-menu-list').removeAttribute('style');
-    document.getElementById('navigation-menu-icon').removeAttribute('style');
-  }
+  closeAccountMenu();
 
-  else
+  document.getElementById('navigation-menu-icon').removeEventListener('click', openMenu);
+
+  $(document.getElementById('navigation-menu-list')).slideDown(250, () =>
   {
-    document.getElementById('navigation-menu-list').style.display = 'block';
     document.getElementById('navigation-menu-icon').style.color = '#7EDA7E';
-  }
+
+    document.getElementById('navigation-menu-icon').addEventListener('click', closeMenu);
+  });
 }
 
 /****************************************************************************************************/
 
-function openOrCloseApps(event)
+function closeMenu(event)
 {
-  if(document.getElementById('navigation-apps-list').hasAttribute('style'))
+  document.getElementById('navigation-menu-icon').removeEventListener('click', closeMenu);
+
+  if(document.getElementById('navigation-apps-icon'))
   {
-    document.getElementById('navigation-apps-icon').removeAttribute('style');
-    document.getElementById('navigation-apps-icon').children[1].innerHTML = `<i class='fas fa-angle-down'></i>`;
-    document.getElementById('navigation-apps-list').removeAttribute('style');
+    closeMenuAppsList();
   }
 
-  else
+  $(document.getElementById('navigation-menu-list')).slideUp(250, () =>
+  {
+    document.getElementById('navigation-menu-icon').removeAttribute('style');
+
+    document.getElementById('navigation-menu-icon').addEventListener('click', openMenu);
+  });
+}
+
+/****************************************************************************************************/
+
+function openMenuAppsList()
+{
+  document.getElementById('navigation-apps-icon').removeEventListener('click', openMenuAppsList);
+
+  $(document.getElementById('navigation-apps-list')).slideDown(100, () =>
   {
     document.getElementById('navigation-apps-icon').style.color = '#428BCA';
     document.getElementById('navigation-apps-icon').children[1].innerHTML = `<i class='fas fa-angle-up'></i>`;
-    document.getElementById('navigation-apps-list').style.display = 'block';
-  }
+
+    document.getElementById('navigation-apps-icon').addEventListener('click', closeMenuAppsList);
+  });
+}
+
+/****************************************************************************************************/
+
+function closeMenuAppsList()
+{
+  document.getElementById('navigation-apps-icon').removeEventListener('click', closeMenuAppsList);
+
+  $(document.getElementById('navigation-apps-list')).slideUp(100, () =>
+  {
+    document.getElementById('navigation-apps-icon').removeAttribute('style');
+    document.getElementById('navigation-apps-icon').children[1].innerHTML = `<i class='fas fa-angle-down'></i>`;
+
+    document.getElementById('navigation-apps-icon').addEventListener('click', openMenuAppsList);
+  });
 }
 
 /****************************************************************************************************/
